4956e5c96625e2841feb77d4292900d14543bb1b
DeSchaker <Robbert.Westerink@gmail.com>
2016-06-01 14:20:40 +0200

    Plot2 script and corresponding plot2.png

---
+++ Plot2.R
@@ -0,0 +1,30 @@
+# Script to read the dataset and output plot2.png
+
+# Initialize 
+inputfile <- "household_power_consumption.txt"
+outputfile <- "plot2.png"
+workingdirectory <- "~/R/Projecten/Coursera/ExData_Plotting1"
+
+Sys.setlocale("LC_TIME", "English")  # Otherwise I get dutch day names in plot output
+
+# Set working directory
+setwd(workingdirectory)
+
+# Read the dataset
+mydata <- read.table(inputfile,na.strings="?",sep=";",header=TRUE,stringsAsFactors=FALSE)
+
+# only keep 2017-02-01 and 2017-02-02
+mydata <- mydata[mydata$Date == "1/2/2007" | mydata$Date == "2/2/2007", ]
+
+# Convert Date and Time string to POSIXct DateTime format
+mydata$DateTime <- as.POSIXct(paste(mydata$Date,mydata$Time),format="%d/%m/%Y %H:%M:%S")
+mydata$Global_active_power <- as.numeric(mydata$Global_active_power)
+
+
+
+png(outputfile,width=480,height=480)
+
+with(mydata,plot(DateTime,Global_active_power,type="n",xlab="",ylab="Global Active Power(kilowatts)" ))
+with(mydata,lines(DateTime,Global_active_power,pch=1,cex=0.2))
+
+dev.off()
